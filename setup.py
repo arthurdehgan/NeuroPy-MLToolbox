fd03a8501373d29f33f41b1159249440b5cbd072
arthurdehgan <arthurdehgan@github.com>
2019-06-11 10:35:56 -0400

    account for update

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup
 
 setup(
     name="mlneurotools",
-    version="0.1",
+    version="0.1.1",
     description="Tools for stats and ml",
     url="http://github.com/arthurdehgan/NeuroPy-MLToolbox",
     author="Arthur Dehgan",
     author_email="arthur.dehgan@umontreal.ca",
-    license="",
     packages=["mlneurotools"],
     zip_safe=False,
 )
