acd7b99b19ac69c3bf4cf13f701963bcf3a2c3d3
arthurdehgan <arthurdehgan@gmail.com>
2021-10-04 14:52:01 -0400

    corrected import bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mlneurotools",
-    version="0.2.5",
+    version="0.2.7",
     description="Tools for stats and ml",
     url="http://github.com/arthurdehgan/NeuroPy-MLToolbox",
     author="Arthur Dehgan",
