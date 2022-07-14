7324bf79d830b7563e6fe5046d9d4fb6330f84bf
Arthur Dehgan <arthurdehgan@gmail.com>
2022-07-14 16:27:11 -0400

    corrected version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mlneurotools",
-    version="0.2.11",
+    version="0.2.13",
     description="Tools for stats and ml",
     url="http://github.com/arthurdehgan/NeuroPy-MLToolbox",
     author="Arthur Dehgan",
